eed0290b8d1e8435853f32d1dd1e1eccb9d1d8de
Jan Švábík <jansvabik@jansvabik.cz>
2020-12-01 21:09:04 +0100

    feat: add day 1 code

---
+++ day-1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// open file with the input values
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Cannot open the input file.")
+	}
+	defer file.Close()
+
+	// open a scanner to read from the file
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	// create an array of values
+	var numbers []int
+	for scanner.Scan() {
+		val, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, val)
+	}
+
+	// test every combination of values
+	stop := false
+	for _, a := range numbers {
+		for _, b := range numbers {
+			if a+b == 2020 {
+				fmt.Println(a * b)
+				stop = true
+			}
+
+			// break the loop if the values were found
+			if stop {
+				break
+			}
+		}
+
+		// break the loop if the values were found
+		if stop {
+			break
+		}
+	}
+}
