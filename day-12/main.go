2957b1fc20261a718e69eae7f4eee3e63c0cc925
Jan Švábík <jansvabik@jansvabik.cz>
2020-12-12 15:52:26 +0100

    feat: add day 12/2 code

--- day-12/main.go
+++ day-12/main.go
@@ -35,6 +35,25 @@ func getNewFacing(curr rune, rot rune, angle int) rune {
 	return faces[index]
 }
 
+// rotatePoint rotates the point around the ship
+// the point coordinates are relatives from the ship
+func rotatePoint(p map[rune]int, rot rune, angle int) map[rune]int {
+	nextSide := map[rune]rune{'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
+	prevSide := map[rune]rune{'N': 'W', 'E': 'N', 'S': 'E', 'W': 'S'}
+	for i := 0; i < angle/90; i++ {
+		newPoint := make(map[rune]int, 4)
+		for k, v := range p {
+			if rot == 'R' {
+				newPoint[nextSide[k]] = v
+			} else {
+				newPoint[prevSide[k]] = v
+			}
+		}
+		p = newPoint
+	}
+	return p
+}
+
 func main() {
 	// open file with the input values
 	file, err := os.Open("input.txt")
@@ -47,27 +66,55 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	// create a slice of all commands
-	moves := [4]float64{0, 0, 0, 0}
-	mi := map[rune]int{'N': 0, 'E': 1, 'S': 2, 'W': 3}
-	currentFacing := 'E'
+	// create a slice of all instructions
+	nums := []string{}
 	for scanner.Scan() {
-		val := scanner.Text()
+		nums = append(nums, scanner.Text())
+	}
+
+	// part 1 variables
+	currentFacing := 'E'                               // current ship facing (starting to east)
+	moves1 := [4]float64{0, 0, 0, 0}                   // ship moves during the navigating
+	mi := map[rune]int{'N': 0, 'E': 1, 'S': 2, 'W': 3} // index to the moves array for each side
+
+	// part 2 variables
+	negSide := map[rune]rune{'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'} // negatives of each side
+	wp := map[rune]int{'N': 1, 'E': 10}                              // current waypoint direction
+	moves2 := [4]float64{0, 0, 0, 0}                                 // ship moves during the navigation
+
+	// process the instructions
+	for _, val := range nums {
 		ins := []rune(val[0:1])[0]
 		arg, _ := strconv.Atoi(val[1:])
 
-		// process the instruction
+		// for part 1
 		switch ins {
 		case 'N', 'E', 'S', 'W':
-			moves[mi[ins]] += float64(arg)
+			moves1[mi[ins]] += float64(arg)
 		case 'L', 'R':
 			currentFacing = getNewFacing(currentFacing, ins, arg)
 		case 'F':
-			moves[mi[currentFacing]] += float64(arg)
+			moves1[mi[currentFacing]] += float64(arg)
+		}
+
+		// for part 2
+		switch ins {
+		case 'N', 'E':
+			wp[ins] += arg
+		case 'S', 'W':
+			wp[negSide[ins]] -= arg
+		case 'L', 'R':
+			wp = rotatePoint(wp, ins, arg)
+		case 'F':
+			for k, v := range wp {
+				moves2[mi[k]] += float64(arg * v)
+			}
 		}
 	}
 
-	// calculate the manhattan distance
-	manhattanDist := math.Abs(moves[mi['S']]-moves[mi['N']]) + math.Abs(moves[mi['E']]-moves[mi['W']])
-	fmt.Println(manhattanDist)
+	// calculate the manhattan distance for each puzzle part
+	md1 := math.Abs(moves1[mi['S']]-moves1[mi['N']]) + math.Abs(moves1[mi['E']]-moves1[mi['W']])
+	md2 := math.Abs(moves2[mi['S']]-moves2[mi['N']]) + math.Abs(moves2[mi['E']]-moves2[mi['W']])
+	fmt.Println("Part 1:", md1)
+	fmt.Println("Part 2:", md2)
 }
